d8a56af0a195b4bf37b4e3c62e07852140a2c48e
Rudy <rudyfelerr@gmail.com>
2018-05-13 16:56:22 -0400

    Primer Commit

---
+++ src/main/java/com/izzisoft/proyectodh/ProyectodhApplication.java
@@ -0,0 +1,12 @@
+package com.izzisoft.proyectodh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectodhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectodhApplication.class, args);
+    }
+}
